425e3a089d8d043e83b2fc87fa94dd3bb9d7f630
Roman <roman.paszkow@gmail.com>
2020-09-26 16:18:25 +0200

    done

--- src/main/java/pl/coderslab/beans/BookController.java
+++ src/main/java/pl/coderslab/beans/BookController.java
@@ -3,52 +3,18 @@ package pl.coderslab.beans;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pl.coderslab.dao.BookService;
-//import pl.coderslab.dao.MemoryBookService;
 import pl.coderslab.entity.Book;
 
 import java.util.List;
-import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
 
-//    MemoryBookService memoryBookService;
-//
-//    @Autowired
-//    public BookController(MemoryBookService memoryBookService) {
-//        this.memoryBookService = memoryBookService;
-//    }
-//
-//
-//    @RequestMapping("")
-//    public List<Book> getAllBooks(){
-//        return memoryBookService.getLocalLibrary();
-//    }
-//
-//    @RequestMapping("/{id}")
-//    public Object getSingleBook(@PathVariable long id){
-//        Optional<Book> bookById = memoryBookService.getBookById(id);
-//        if(bookById.isPresent()){
-//            return bookById.get();
-//        }else {
-//            return "No such book";
-//        }
-//    }
-//    @PostMapping("")
-//    public void newBook(@RequestBody Book book) {
-//        memoryBookService.addBooks(book);
-//    }
-//
-//    @PutMapping("")
-//    public void updateBook(@RequestBody Book bookForUpdate){
-//        memoryBookService.editBook(bookForUpdate);
-//    }
-//    @DeleteMapping("/{id}")
-//    public void deleteBook(@PathVariable long id){  }
-
     BookService bookService;
 
+    @Autowired
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
